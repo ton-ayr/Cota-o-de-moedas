8ff903327e31b47530ad13dee8c3945b94ba4cd1
Ayrton Cesar <67876610+ton-ayr@users.noreply.github.com>
2024-11-19 21:43:02 -0300

    Add files via upload

---
+++ Cotação de moedas.py
@@ -0,0 +1,32 @@
+import requests
+from tkinter import *
+
+def pegar_cotacoes():
+    requisicao = requests.get("https://economia.awesomeapi.com.br/last/BRL-USD,USD-BRL,EUR-BRL,BTC-BRL")
+
+    requisicao_dic = requisicao.json()
+
+    cotacao_real = requisicao_dic['BRLUSD']['bid']
+    cotacao_dolar = requisicao_dic['USDBRL']['bid']
+    cotacao_euro = requisicao_dic['EURBRL']['bid']
+    cotacao_btc = requisicao_dic['BTCBRL']['bid']
+
+    texto_resposta['text'] = f'''
+    Real: {cotacao_real}
+    Dólar: {cotacao_dolar}
+    Euro: {cotacao_euro}
+    BTC: {cotacao_btc}'''
+
+janela = Tk()
+janela.title("Cotação de Moedas")
+texto = Label(janela, text="Clique no botão para ver as cotações de moedas")
+texto.grid(column=0, row=0, padx=10, pady=10)
+
+botao = Button(janela, text="Buscar cotações", command=pegar_cotacoes)
+botao.grid(column=0, row=1, padx=10, pady=10)
+
+texto_resposta = Label(janela, text="")
+texto_resposta.grid(column=0, row=2, padx=10, pady=10)
+
+
+janela.mainloop()
